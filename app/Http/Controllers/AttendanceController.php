b0d49b31e20ad892979b7f58a0440976d3a893f5
Rizaldy Wirawan <rizaldy.wirawan@re.rekayasa.co.id>
2022-01-25 07:39:34 +0700

    Add users filter for summary data in attendances page

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -29,9 +29,9 @@ class AttendanceController extends Controller
 
         $attendances = Attendance::whereIn('user_id', $userIDs)->with(['user.profile', 'locationType'])->orderBy('created_at', 'DESC')->get();
 
-        $attendanceToday = Attendance::whereBetween('created_at', [now()->isoFormat('YYYY-MM-DD 00:00:00.000'), now()->isoFormat('YYYY-MM-DD 23:59:00.000')])->count();
-        $attendanceByWebsite = Attendance::where('location_type_id', 'dd00a679-f0f0-45ae-9ab3-60baabcbbd67')->count();
-        $attendanceByQRCode = Attendance::where('location_type_id', '523c9f80-7a68-4f9a-85f1-f1597d65a513')->count();
+        $attendanceToday = Attendance::whereIn('user_id', $userIDs)->whereBetween('created_at', [now()->isoFormat('YYYY-MM-DD 00:00:00.000'), now()->isoFormat('YYYY-MM-DD 23:59:00.000')])->count();
+        $attendanceByWebsite = Attendance::whereIn('user_id', $userIDs)->where('location_type_id', 'dd00a679-f0f0-45ae-9ab3-60baabcbbd67')->count();
+        $attendanceByQRCode = Attendance::whereIn('user_id', $userIDs)->where('location_type_id', '523c9f80-7a68-4f9a-85f1-f1597d65a513')->count();
 
 
         $businessUnits = BusinessUnit::with('company')
