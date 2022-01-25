fd198716fd35ed9667e1f96bd70836d4c856d9ae
Rizaldy Wirawan <rizaldy.wirawan@re.rekayasa.co.id>
2022-01-25 07:38:29 +0700

    Fix attendance user filter.

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -13,17 +13,26 @@ class AttendanceController extends Controller
 
     public function index()
     {
+        $totalUser = User::whereHas('roles', function($query) {
+            $query->where('name', 'participant');
+        })
+        ->count();
 
-        $attendances = Attendance::with(['user.profile', 'locationType'])->orderBy('created_at', 'DESC')->get();
+        $users = User::whereHas('roles', function($query) {
+            $query->where('name', 'participant');
+        })
+        ->get()
+        ->toArray();
+
+        $userIDs = array_column($users, 'id');
+
+
+        $attendances = Attendance::whereIn('user_id', $userIDs)->with(['user.profile', 'locationType'])->orderBy('created_at', 'DESC')->get();
 
         $attendanceToday = Attendance::whereBetween('created_at', [now()->isoFormat('YYYY-MM-DD 00:00:00.000'), now()->isoFormat('YYYY-MM-DD 23:59:00.000')])->count();
         $attendanceByWebsite = Attendance::where('location_type_id', 'dd00a679-f0f0-45ae-9ab3-60baabcbbd67')->count();
         $attendanceByQRCode = Attendance::where('location_type_id', '523c9f80-7a68-4f9a-85f1-f1597d65a513')->count();
 
-        $totalUser = User::whereHas('roles', function($query) {
-            $query->where('name', 'participant');
-        })
-        ->count();
 
         $businessUnits = BusinessUnit::with('company')
         ->withCount(['participants'])
